f5811be471042bb08b0edd47ea464e4954120593
Nelson, Scott <snelson@collegeboard.org>
2024-03-07 13:31:17 -0800

    added ui and logic for entering tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
+import { useState, useEffect } from "react";
+import TaskInput from "./components/TaskInput";
+import { Task } from "./types/Task";
+
 function App() {
+  const [tasks, setTasks] = useState<Task[]>([]);
 
   return (
-    <>
-      App goes here
-    </>
-  )
+    <div className="w-screen h-screen mt-8 flex flex-col items-center">
+      <h1 className="text-center uppercase z-10 mb-8 font-[Neucha] text-4xl font-bold">
+        Taskmaster
+      </h1>
+      <TaskInput tasks={tasks} setTasks={setTasks}/>
+      <ul>
+        {tasks.map((task, index) => (<li key={index}>{task.description}</li>))}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/TaskInput.tsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import { Task } from "../types/Task";
+
+interface Props {
+  tasks: Task[],
+  setTasks: React.Dispatch<React.SetStateAction<Task[]>>
+}
+
+function TaskInput({tasks, setTasks}: Props) {
+  const [task, setTask] = useState<string>("");
+  const [formError, setFormError] = useState("");
+
+  const addTask = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if(task !== ""){
+      const item: Task = {
+        description: task,
+        isComplete: false
+      };
+
+      for(let i = 0; i < tasks.length; i++){
+        if(tasks[i].description === task){
+          setFormError("Task already exists");
+          return;
+        }
+      }
+
+      setTasks((tasks) => [...tasks, item]);
+    }
+
+    setTask("");
+    setFormError("");
+  }
+
+  return (
+    <form className="flex flex-col w-[90%] relative items-center" onSubmit={addTask}>
+      <input
+        type="text"
+        placeholder="Enter a task"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+        className="w-full rounded-full pt-5 pb-5 pl-8 pr-8 text-2xl text-gray-500 border-none transition-[0.2s] shadow-[inset_0_0_5px_black] focus:outline-none focus:shadow-[0_0_10px_1000px_rgba(0,0,0,0.5)]"
+      />
+      <button type="submit" className="absolute right-0 w-12 h-12 bg-blue-400 border-none rounded-full m-3 text-white text-lg transition-[0.2s_all] shadow-[0_0_10px_black] hover:bg-blue-500 active:scale-[0.8] active:shadow-[0_0_5px_black]">Go</button>
+      {formError && <p className="text-xl text-red-500 font-bold uppercase">{formError}</p>}
+    </form>
+  );
+}
+
+export default TaskInput;
